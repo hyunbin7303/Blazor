25d692a2ef1ed4a033a4666d77433707be56023a
kevin <hyunbin7303@gmail.com>
2020-11-11 00:35:11 -0500

    Update Validation Input.

---
+++ Blazor_Demo/Blazor_Demo/Client/Controls/ValidationInputBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Demo.Client.Controls
+{
+    public class ValidationInputBase : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+            
+        [Parameter]
+        public string Value { get; set; }
+
+
+        protected async void HandleInputChanged(ChangeEventArgs args)
+        {
+            await ValueChanged.InvokeAsync(args.Value.ToString());
+        }
+    }
+}
