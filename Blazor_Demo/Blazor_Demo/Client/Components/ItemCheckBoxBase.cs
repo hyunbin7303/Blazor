6dd8f55abef46be78202555eefad6124c381deab
kevin <hyunbin7303@gmail.com>
2020-11-21 22:27:56 -0500

    Adding Checkbox.

---
+++ Blazor_Demo/Blazor_Demo/Client/Components/ItemCheckBoxBase.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_Demo.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Demo.Client.Components
+{
+    public class ItemCheckBoxBase : ComponentBase
+    {
+        [Parameter]
+        public BaseItem Item { get; set; }
+    }
+}
