99135a292cad6223b3e21186a432141c0b888f8f
kevin <hyunbin7303@gmail.com>
2020-11-21 22:05:31 -0500

    Updating ItemElement  + css files

---
+++ Blazor_Demo/Blazor_Demo/Client/Components/ItemElement.razor.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_Demo.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Demo.Client.Components
+{
+    public partial class ItemElement<Titem> : ComponentBase where Titem : BaseItem
+    {
+        [Parameter]
+        public Titem Item { get; set; }
+    }
+}
